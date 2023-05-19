ebd837a825a8337cc4cfda0f4a80fd942abf4163
theblackman <pascal@kinshasadigital.com>
2023-05-19 18:46:50 +0100

    fix🐛: transform text to uppercase nefore check

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 0.0.1
 
 - Initial version with basic word check
+
+## 0.0.2
+
+- Fix🐛: transform text to toUpperCase before check
--- lib/src/main.dart
+++ lib/src/main.dart
@@ -2,7 +2,7 @@ import './words.dart';
 
 class ScrabbleWordChecker {
   bool isValidWord(String word) {
-    if (words.contains(word)) {
+    if (words.contains(word.toUpperCase())) {
       return true;
     }
     return false;
