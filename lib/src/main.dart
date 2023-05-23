16650066831b2c8e1da2000680101c53acb25556
theblackman <pascal@kinshasadigital.com>
2023-05-23 11:50:31 +0100

    fix🐛: pass all tests

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -11,6 +11,19 @@ class ScrabbleWordChecker {
     this.language = ScrabbleLanguage.french,
   });
 
+  /// Checks for a valid and acceptable word
+  ///
+  /// A given word can't have spaces, numbers and special chars
+  ///
+  /// Ex: - Black Man: invalid
+  ///     - Black123: invalid
+  ///     - BlacK: valid
+  ///     - 123: invalid
+  bool _isAcceptableWord(String word) {
+    final RegExp alphaRegexp = RegExp(r'^[a-zA-Z]+$');
+    return alphaRegexp.hasMatch(word);
+  }
+
   /// Change the checker language
   /// Default language is french
   ///
@@ -27,6 +40,9 @@ class ScrabbleWordChecker {
   ///
   /// To change the language, call the [changeLanguage] function
   bool isValidWord(String word) {
+    if (!_isAcceptableWord(word)) {
+      return false;
+    }
     switch (language) {
       case ScrabbleLanguage.english:
         return englishWords.contains(word);
@@ -41,6 +57,9 @@ class ScrabbleWordChecker {
   ///
   /// Takes a word as parametter
   int getWordValue(String word) {
+    if (!_isAcceptableWord(word)) {
+      return 0;
+    }
     int sum = 0;
     for (int i = 0; i < word.length; i++) {
       final int value = letterValues[word[i].toLowerCase()] ?? 0;
